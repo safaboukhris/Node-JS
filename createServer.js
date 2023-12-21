08199fe28053708f9bfab6156a2482bacccdddb9
safa <safaboukhris@yahoo.fr>
2023-12-21 08:50:21 +0100

    secondCommit

--- createServer.js
+++ createServer.js
@@ -3,7 +3,7 @@ const http = require('http');
         //create a server
 const server = http.createServer((request,response)=>{
     response.writeHead(200,'OK',{'content-type': 'text/html'});
-    response.end('<h1>HELLO WORLD</h1>');
+    response.end('<h1>HELLO WORLD</h1>\n');
 })
 
 server.listen(3000,()=>{
